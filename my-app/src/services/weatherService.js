a378916298b1f687f028fe5cb5b3a44fd15faa05
osho80 <omh1980@gmail.com>
2021-09-14 13:16:30 +0300

    back to axios

--- my-app/src/services/weatherService.js
+++ my-app/src/services/weatherService.js
@@ -14,24 +14,24 @@ const BASE_URL =
 // };
 
 // export const queryCity = async (q) => await getReq(`/cities`, q);
-const xhr = new XMLHttpRequest();
+// const xhr = new XMLHttpRequest();
 
 export const queryCity = async (q) => {
   console.log("Querying city");
   try {
     const x = BASE_URL + "cities/" + q;
     console.log("My x:", x);
-    // const res = await axios.get(`${x}`);
-    // console.log("My res:", res);
-    xhr.onreadystatechange = () => {
-      if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-        const res = JSON.parse(xhr.responseText);
-        console.log("My xhr res:", res);
-      }
-    };
+    const res = await axios.get(`${x}`);
+    console.log("My res:", res);
+    // xhr.onreadystatechange = () => {
+    //   if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+    //     const res = JSON.parse(xhr.responseText);
+    //     console.log("My xhr res:", res);
+    //   }
+    // };
 
-    xhr.open("Get", x);
-    xhr.send();
+    // xhr.open("Get", x);
+    // xhr.send();
   } catch (err) {
     console.log("Bad request!!!!!");
     console.dir(err);
